81a86f9c541e85b656ffd411e2b34e787bf9ea34
Jemma Tiongson <jemmationgson.dev@gmail.com>
2023-05-23 20:12:46 -0700

    initial push

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
